e18828b1ca71010a6575c4894e2c18ff7d1216dd
Sandeep Raj B <36770496+betanapallisandeepraj@users.noreply.github.com>
2021-04-14 14:30:23 +0800

    add PS1='[\D{%H:%M:%S-%d-%m-%Y}]\u@\h:\w$' alone

--- lsp/print_time_on_terminal.sh
+++ lsp/print_time_on_terminal.sh
@@ -1,3 +1,4 @@
 #run below command in terminal or execute this script to sustain the time. 
 #add below line in .bashrc to sustain time even after reboot.
-export PS1='[\D{%H:%M:%S-%d-%m-%Y}]\u@\h:\w$'
+#export PS1='[\D{%H:%M:%S-%d-%m-%Y}]\u@\h:\w$'
+PS1='[\D{%H:%M:%S-%d-%m-%Y}]\u@\h:\w$'
