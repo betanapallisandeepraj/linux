a4eb5a41adb7eec9f61739be5e47b2abd6ce6e74
Sandeep Raj B <36770496+betanapallisandeepraj@users.noreply.github.com>
2021-04-14 14:23:40 +0800

    Create print_time_on_terminal.sh

---
+++ lsp/print_time_on_terminal.sh
@@ -0,0 +1,3 @@
+#run below command in terminal or execute this script to sustain the time. 
+#add below line in .bashrc to sustain time even after reboot.
+export PS1='[\D{%H:%M:%S-%d-%m-%Y}]\u@\h:\w$'
